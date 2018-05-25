fb312fcfede030234e8ca6df9144dabaf1b7c39f
JJ Herasymchuk <evhenii.herasymchuk@gmail.com>
2018-05-25 21:10:09 +0200

    Add DayHeader component

--- src/components/CalendarDay/CalendarDay.js
+++ src/components/CalendarDay/CalendarDay.js
@@ -1,10 +1,15 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import DayHeader from './DayHeader/DayHeader';
 
 import './CalendarDay.css';
 
 const CalendarDay = () => {
-  return <div className="rt-calendar-day" />;
+  return (
+    <div className="rt-calendar-day">
+      <DayHeader />
+    </div>
+  );
 };
 
 CalendarDay.propTypes = {};
---
+++ src/components/CalendarDay/DayHeader/DayHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './DayHeader.css';
+
+const DayHeader = () => {
+  return (
+    <div className="rt-calendar-day-header">
+    </div>
+  );
+};
+
+DayHeader.propTypes = {};
+
+export default DayHeader;
