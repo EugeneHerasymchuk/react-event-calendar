f2cea51599081ad1a307d940037fd0140c1207f3
JJ Herasymchuk <evhenii.herasymchuk@gmail.com>
2018-05-27 20:53:00 +0200

    Eject the project

--- src/components/CalendarDay/DayHeader/DayHeader.test.js
+++ src/components/CalendarDay/DayHeader/DayHeader.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import DayHeader from './DayHeader.js';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { expect } from 'jest';
 
 configure({ adapter: new Adapter() });
 
@@ -11,7 +10,7 @@ describe('DayHeader', () => {
     const title = 'myDayName';
     const wrapper = shallow(<DayHeader name={title} />);
 
-    expect(wrapper.text()).equal(title);
+    expect(wrapper.text()).toEqual(title);
   });
   // it('should throw error if the title of the Day is missing', () => {
   //
